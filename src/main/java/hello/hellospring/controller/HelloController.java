cf34ae7f399fd77496c61e79f596ce5095693ef2
Sungwook Park <sungwook1983@gmail.com>
2022-08-09 21:05:40 +0900

    FINAL

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,12 +1,10 @@
 package hello.hellospring.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 public class HelloController {
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,21 +1,62 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@SpringBootTest
+@Transactional
 class MemoryMemberRepositoryTest {
 
-    MemoryMemberRepository repository = new MemoryMemberRepository();
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //Given
+        Member member = new Member();
+        member.setName("hello");
+        //When
+        Long saveId = memberService.join(member);
+        //Then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
+    }
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //Given
+        Member member1 = new Member();
+        member1.setName("spring");
+        Member member2 = new Member();
+        member2.setName("spring");
+        //When
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+
+    /*
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
 
     @AfterEach
     public void afterEach() {
-        repository.clearStore();
+        memberRepository.clearStore();
     }
 
     @Test
@@ -23,9 +64,9 @@ class MemoryMemberRepositoryTest {
         Member member = new Member();
         member.setName("spring");
 
-        repository.save(member);
+        memberRepository.save(member);
 
-        Member result = repository.findById(member.getId()).get();
+        Member result = memberRepository.findById(member.getId()).get();
         assertThat(result).isEqualTo(member);
     }
 
@@ -33,13 +74,13 @@ class MemoryMemberRepositoryTest {
     public void findByName() {
         Member member1 = new Member();
         member1.setName("spring1");
-        repository.save(member1);
+        memberRepository.save(member1);
 
         Member member2 = new Member();
         member2.setName("spring2");
-        repository.save(member2);
+        memberRepository.save(member2);
 
-        Member result = repository.findByName("spring2").get();
+        Member result = memberRepository.findByName("spring2").get();
 
         assertThat(result).isEqualTo(member2);
     }
@@ -48,15 +89,16 @@ class MemoryMemberRepositoryTest {
     public void findAll() {
         Member member1 = new Member();
         member1.setName("spring1");
-        repository.save(member1);
+        memberRepository.save(member1);
 
         Member member2 = new Member();
         member2.setName("spring2");
-        repository.save(member2);
+        memberRepository.save(member2);
 
-        List<Member> result = repository.findAll();
+        List<Member> result = memberRepository.findAll();
 
         assertThat(result.size()).isEqualTo(2);
     }
+    */
 
 }
